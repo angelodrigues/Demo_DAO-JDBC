1807cb493090675da017f57b150f2bde1cf55192
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-08 16:35:04 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,5 @@
 package application;
 
-import java.util.Date;
-
-import model.Department;
 import model.Seller;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,12 +7,10 @@ import model.dao.SellerDao;
 public class Program {
     public static void main(String[] args) throws Exception {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "Angelo", "angelo@gmail.com", new Date(), 2000.0, obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
