98ec5b27f2e3dba8b4382c7d91fe7a9598ebf9ce
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-08 17:14:46 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.Department;
@@ -31,5 +32,10 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+        System.out.println("\n=== TESTE 3: Seller findAll ===");        
+        Seller newSeller = new Seller(null, "Yasmim", "yasmim@gmail.com", new Date(), 5000.0, department);
+
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
